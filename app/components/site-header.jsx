8cb025173a01557a8995feca49279419431264e3
Nahum Portillo <nahum_137@hotmail.com>
2026-08-21 13:27:23 -0700

    feat: build responsive event details and shared site footer

--- app/components/site-header.jsx
+++ app/components/site-header.jsx
@@ -43,16 +43,31 @@ const primaryNavigation = [
 export default function SiteHeader() {
   return (
     <header className="border-t-4 border-brand bg-black">
-      <div className="mx-auto flex h-[7.1875rem] max-w-[70rem] items-center px-3">
+      <div className="mx-auto hidden h-[7.1875rem] max-w-[70rem] items-center px-3 min-[48.0625rem]:flex">
         <SiteLogo />
       </div>
 
+      <div className="relative flex h-[4.25rem] items-center justify-center px-4 min-[48.0625rem]:hidden">
+        <button
+          type="button"
+          className="absolute left-4 top-1/2 flex -translate-y-1/2 flex-col gap-1 rounded-sm p-2 focus-visible:outline-3 focus-visible:outline-offset-2 focus-visible:outline-brand-strong"
+          aria-label="Open menu"
+        >
+          <span className="block h-1 w-7 bg-white" />
+          <span className="block h-1 w-7 bg-white" />
+          <span className="block h-1 w-7 bg-white" />
+        </button>
+        <div className="scale-[0.6]">
+          <SiteLogo />
+        </div>
+      </div>
+
       <nav
-        className="border-t border-white/5"
+        className="hidden border-t border-white/5 min-[48.0625rem]:block"
         aria-label="Primary navigation"
       >
         <div className="mx-auto max-w-[70rem] overflow-x-auto px-3 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-          <ul className="flex h-[5.625rem] w-max min-w-full items-center justify-start p-0 lg:pl-[2.1875rem]">
+          <ul className="flex h-[4.25rem] w-max min-w-full items-center justify-start p-0 lg:pl-[2.1875rem]">
             {primaryNavigation.map((item) => (
               <li className="shrink-0" key={item.label}>
                 <a
